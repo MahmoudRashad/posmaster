2c8e13f68bb95ccd5f2bb7da06941de1c8972a22
MahmoudRashad <mahmoud.rashad7010@gmail.com>
2018-03-15 01:17:23 +0200

    15-3-2018 commit
    Signed-off-by: MahmoudRashad <mahmoud.rashad7010@gmail.com>

--- app/src/main/java/com/refresh/pos/ui/LoginActivity.java
+++ app/src/main/java/com/refresh/pos/ui/LoginActivity.java
@@ -2,6 +2,7 @@ package com.refresh.pos.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +12,17 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.refresh.pos.R;
 import com.refresh.pos.domain.Branche;
+import com.refresh.pos.domain.LanguageController;
 import com.refresh.pos.networkmanger.Get_Branches_Manger;
 import com.refresh.pos.networkmanger.LogInManager;
 import com.refresh.pos.techicalservices.Globalclass;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -27,7 +31,9 @@ public class LoginActivity extends Activity {
 
 
     Spinner mySpinner;
+    Spinner mySpinner_lang;
     ArrayList<Branche> branches_list_s;
+    boolean selct_lang = false;
     private EditText Email;
     private EditText password;
     private Button skipbtn;
@@ -39,36 +45,108 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        setdata();
         initializationUI();
         get_Branches();
+        get_lang();
+
+    }
+
+    private void setdata() {
+        try {
+
+            Email.setText(getIntent().getStringExtra("Email"));
+            password.setText(getIntent().getStringExtra("password"));
+            mySpinner.setSelection(getIntent().getIntExtra("mySpinner_index", 0));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void get_lang() {
+
+
+        mySpinner_lang = findViewById(R.id.lang_sp);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.lang));
+        mySpinner_lang.setAdapter(spinnerArrayAdapter);
+        mySpinner_lang.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selct_lang = i != 0;
+                if (i == 1)
+                    setLanguage("ar");
+                else if (i == 2)
+                    setLanguage("en");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    }
+
+    /**
+     * Set language
+     *
+     * @param localeString
+     */
+    private void setLanguage(String localeString) {
+        Locale locale = new Locale(localeString);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        LanguageController.getInstance().setLanguage(localeString);
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+        Intent intent = getIntent();
+        intent.putExtra("Email", Email.getText().toString());
+        intent.putExtra("password", password.getText().toString());
+        intent.putExtra("mySpinner_index", mySpinner.getSelectedItemPosition());
+
+
+        finish();
+        startActivity(intent);
     }
 
     private void get_Branches() {
+        final SweetAlertDialog pDialog = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
 
         Get_Branches_Manger get_branches_manger= new Get_Branches_Manger(LoginActivity.this);
         get_branches_manger.setCustomObjectListener(new Get_Branches_Manger.MyCustomObjectListener() {
             @Override
             public void onObjectReady(ArrayList<Branche> branches_list) {
+
                 branches_list_s=branches_list;
+                Branche branche = new Branche("-1", getResources().getString(R.string.selectoffice));
+                branches_list_s.add(0, branche);
                 ArrayAdapter<Branche> adapter =
                         new ArrayAdapter<Branche>(getApplicationContext(),
                                 android.R.layout.simple_spinner_dropdown_item,
                                 branches_list);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 mySpinner.setAdapter(adapter);
+                pDialog.dismiss();
 
             }
 
             @Override
             public void onFailed(String title) {
-
+                pDialog.dismiss();
                 Log.e("onFailed:getbranches ",title );
 
             }
         });
-      get_branches_manger.get_Branches();
-
-
+        get_branches_manger.get_Branches();
 
 
     }
@@ -189,6 +267,12 @@ public class LoginActivity extends Activity {
             password.setError(getString(R.string.errorpassword));
             return false;
         }
+        if (mySpinner.getSelectedItem() == null || ((Branche) mySpinner.getSelectedItem()).getOfficeId() == "-1") {
+            ((TextView) mySpinner.getSelectedView()).setError(getString(R.string.errorpassword));
+            return false;
+        }
+
+
 
         return true;
     }
